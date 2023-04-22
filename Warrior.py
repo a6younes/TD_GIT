379ad0be065f304ea9778f9d05bcb643127e00ca
a6younes <y.assaouci@gmail.com>
2023-04-22 21:20:18 +0200

    J'ai crée un warrior

---
+++ Warrior.py
@@ -0,0 +1,10 @@
+import random
+
+class Warrior:
+    def __init__(self):
+        self.damage = random.randint(3, 5)
+        self.schance = 5
+        self.escape = 3
+        self.price = 10
+        self.unit_type = "warrior"
+
