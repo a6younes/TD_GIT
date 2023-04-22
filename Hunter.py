574ce3b8554cf762bd5675c88e717a46d2d02d6d
Younes A <91132589+a6younes@users.noreply.github.com>
2023-04-22 21:26:10 +0200

    Delete Hunter.py

--- Hunter.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Hunter:
-    def __init__(self):
-        self.damage = random.randint(1, 2)
-        self.chance = 10
-        self.escape = 20
-        self.price = 25
-        self.unit_type = "hunter"
