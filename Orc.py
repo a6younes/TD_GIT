d68e1d86b850c1d30b6f9b17505dc36d476af2f4
Setha77 <setha_hong77@hotmail.com>
2023-04-22 21:43:14 +0200

    j'ai créer un Orc

---
+++ Orc.py
@@ -0,0 +1,8 @@
+class Orc: 
+    def __init__(self, degat, loot):
+        self.degat = degat
+        self.loot = loot
+    
+    def randomDegat(self):
+        self.degat = random.choice(range(3, 5))
+        self.loot = random.choice(range(2, 2.5))
